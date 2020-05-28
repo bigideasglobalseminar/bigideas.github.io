cf04d35132b2a2c15393ae75dc82a20b51be2095
Shannon Ding <shannon_ding@yahoo.com>
2020-05-27 20:26:47 -0400

    mobile nav for edge

--- scripts/slide-it.js
+++ scripts/slide-it.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
   $('.team-slide').slick({
   	infinite: true,
-  	dots: true
+  	dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
   });
   $('.land-image-slide').slick({
   	infinite: true,
